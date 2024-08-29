bac3347e53dab7a5e68daa4477ff0dc16512ee31
pradhanmitu <166330938+pradhanmitu@users.noreply.github.com>
2024-08-29 22:45:03 +0530

    Add files via upload

---
+++ Process.java
@@ -0,0 +1,15 @@
+package ring;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Process{
+    String id;
+    Process hope;
+    Queue<Process> request_q = new LinkedList<>();
+
+    public Process(String id){
+        this.id = id;
+    }
+}
---
+++ RingTopology.java
@@ -0,0 +1,44 @@
+package ring;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RingTopology extends Thread{
+    static public ArrayList<Process> processes = new ArrayList<>();
+    static public Process token = null;
+    static public boolean cs = false;
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Users\\Mitu Pradhan\\Desktop\\Java\\operating system\\ring\\src\\ring\\Processes.txt");
+        try (Scanner sc = new Scanner(file)) {
+            while(sc.hasNext()){
+                String p = sc.next();
+                Process process = new Process(p);
+                processes.add(process);
+            }
+        }
+
+        for(int i = 0;i<processes.size();i++){
+            if(i == (processes.size()-1)){
+                processes.get(i).hope = processes.get(0);
+            }
+            else{
+                processes.get(i).hope = processes.get(i+1);
+            }
+        }
+
+        int index = (int)(Math.random()*processes.size());
+        token = processes.get(index);
+        System.out.println("Token Holder Process "+token.id);
+
+        Thread t1 = new RequestCriticalSection();
+        t1.start();
+        Thread t2 = new RequestCriticalSection();
+        t2.start();
+        // Thread t3 = new RequestCriticalSection();
+        // t3.start();
+
+    }
+}
+
